b4cf3fe110849716adc20df3b56dbc2517fb73f5
Ethan <ethanstm8@gmail.com>
2021-05-31 00:37:01 -0400

    I'm gonna just save my work before I go to bed

--- cpu.cpp
+++ cpu.cpp
@@ -1,3 +1,32 @@
 #include "cpu.h"
 
-void CPU::executeCycle() {};
+constexpr memSize = 65536;
+ 
+void initialize() {
+	PC = 0x0200;
+	SP = 0x00;
+	AC = 0x00;
+	X =  0x00;
+	Y =  0x00;
+	ST = 0x00;
+	memory = std::vector<uint8_t>(memSize);
+}
+//Implement later
+void loadProgram(std::string const& name) {};
+
+
+void CPU::executeCycle() {
+
+	switch(memory[PC]) {
+		/*Try optimizing this by making
+		the accumulator in a union with the
+		carry bit, so you only have to do one
+		instruction!*/
+		
+		//ADC Instructions
+		case 0x69: {
+			
+		}
+
+	}
+};
--- cpu.h
+++ cpu.h
@@ -7,8 +7,7 @@ class CPU {
 	static constexpr uint16_t stackStart = 0x0100;
 	static constexpr uint16_t stackEnd =   0x01FF;
 
-	//Why not have the program memory
-	uint16_t PC = 0x0200;
+	uint16_t PC = stackEnd+1;
 	/*SP is an offset from 0x0100, not an
 	absolute address, since the SP is only 8 bit*/
 	uint8_t SP =  0x00;
@@ -40,8 +39,12 @@ class CPU {
 	//Don't use a separate memory class for now
 	std::vector<uint8_t> memory;
 
+	//Wipe all registers and memory
+	void initialize();
 
 public:
+	CPU();
 
+	void loadProgram(std::string const& name);
 	void executeCycle();
 };
